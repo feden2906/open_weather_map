d75a870fe3f5d4f1578f9b112ea050a1be9ca58d
feden2906 <feden2906@gmail.com>
2021-09-24 18:42:12 +0300

    add getCurrentPosition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { Header, Weather_now } from "./components";
+import { useEffect, useState } from "react";
 
 export const App = () => {
+  const [state, setState] = useState(null);
 
+  useEffect(async () => {
+    await navigator.geolocation.getCurrentPosition(
+        position => {
+          setState({ latitude: position.coords.latitude, longitude: position.coords.longitude })
+        }
+    )
+  }, [])
+  console.log(state)
     return (
         <div>
             <Header/>
